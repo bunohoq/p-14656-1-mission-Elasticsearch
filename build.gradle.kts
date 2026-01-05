8eeab508a3a16efc814ed347c7c0b40817702811
박보영 <bunohoq@parkboyoung-ui-MacBookAir.local>
2026-01-05 16:07:59 +0900

    0007, 0008 : bassInit 클래스, lombok 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,11 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-data-elasticsearch-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
